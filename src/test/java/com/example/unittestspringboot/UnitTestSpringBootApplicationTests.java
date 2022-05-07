93661633a4bd7cd01c43201c0bc9f328766196f4
rakibccj <rakibccj@gmail.com>
2022-05-07 22:07:40 +0600

    project initiate

---
+++ src/test/java/com/example/unittestspringboot/UnitTestSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.unittestspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnitTestSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
